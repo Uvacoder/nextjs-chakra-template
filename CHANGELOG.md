1066e1f8a6fb96c665fadf5b8dd32262a3d4765d
Uvacoder <uva22940coder@gmail.com>
2022-10-27 21:46:45 -0400

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 1.1.0 (2022-07-07)
+
+
+### Features
+
+* update template ([088177e](https://github.com/avneesh0612/next-chakra-template/commit/088177ed4753114f56f0df16015497ee76fecf55))
+
+
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Layout from '@/Layouts/Main.layout';
+import { Link, Text } from '@chakra-ui/react';
+import { FC } from 'react';
+
+const Home: FC = () => {
+    return (
+        <Layout>
+            <Text as="h2" fontFamily="sans-serif" fontSize="5xl">
+                Next.js + Chakra Starter Template
+            </Text>
+            <Link
+                _hover={{
+                    background: 'blue.500',
+                }}
+                background="blue.600"
+                borderRadius="0.5rem"
+                color="white"
+                href="https://github.com/avneesh0612/next-chakra"
+                isExternal
+                marginTop="1rem"
+                padding="0.5rem 1rem"
+                size="lg"
+                target="_blank"
+            >
+                View repository
+            </Link>
+        </Layout>
+    );
+};
+
+export default Home;
